71b008d9d8ced29e0ea3dc94a09f0203a668b71e
ftsuda-senac <ftsuda.senac@gmail.com>
2020-09-15 22:06:38 -0300

    Exemplos Servlet e JSP

---
+++ exemplos-servlet/src/main/java/br/senac/tads/dsw/exemplos/servlet/Exemplo2Servlet.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.tads.dsw.exemplos.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "Exemplo2Servlet", urlPatterns = {"/exemplo2"})
+public class Exemplo2Servlet extends HttpServlet {
+
+  
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        String param = request.getParameter("param");
+        if ("redirect".equals(param)) {
+            response.sendRedirect(request.getContextPath() + "/resultado-redirect.jsp");
+        } else {
+            RequestDispatcher dispatcher = request.getRequestDispatcher("/resultado-forward.jsp");
+            dispatcher.forward(request, response);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+}

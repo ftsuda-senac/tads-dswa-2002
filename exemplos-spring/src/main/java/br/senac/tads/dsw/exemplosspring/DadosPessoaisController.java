020db67eaab2b294407efc98c355b1971871bbc1
ftsuda-senac <ftsuda.senac@gmail.com>
2020-09-22 22:28:34 -0300

    Exemplo dados pessoais

---
+++ exemplos-spring/src/main/java/br/senac/tads/dsw/exemplosspring/DadosPessoaisController.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.tads.dsw.exemplosspring;
+
+import java.time.LocalDate;
+import java.time.Month;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/dados-pessoais")
+public class DadosPessoaisController {
+
+    @GetMapping("/{apelido}")
+    public ModelAndView mostrarDados(
+            @PathVariable("apelido") String apelido) {
+        String nome = "Seu Madruga";
+        String telefone = "(11) 99999-1234";
+        String email = "madruga@teste.com.br";
+        LocalDate dataNascimento = LocalDate.of(1971, 6, 20);
+        
+        ModelAndView mv = new ModelAndView("dados-pessoais");
+        mv.addObject("nome", nome);
+        mv.addObject("telefone", telefone);
+        mv.addObject("email", email);
+        mv.addObject("dataNascimento", dataNascimento);
+        return mv;
+    }
+    
+}

c830f9077bd23edd0c3a012daae4d7d085d602ff
ftsuda-senac <ftsuda.senac@gmail.com>
2020-10-06 20:55:39 -0300

    Exemplos de uso da Sessão

--- exemplos-spring-sessao/src/main/java/br/senac/tads/dsw/exemplosspring/sessao/ExemploSessaoController2.java
+++ exemplos-spring-sessao/src/main/java/br/senac/tads/dsw/exemplosspring/sessao/ExemploSessaoController2.java
@@ -5,10 +5,13 @@
  */
 package br.senac.tads.dsw.exemplosspring.sessao;
 
+import br.senac.tads.dsw.exemplosspring.sessao.item.Item;
+import br.senac.tads.dsw.exemplosspring.sessao.item.ItemService;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -17,57 +20,53 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import br.senac.tads.dsw.exemplosspring.sessao.item.Item;
-import br.senac.tads.dsw.exemplosspring.sessao.item.ItemService;
-import org.springframework.web.bind.annotation.SessionAttributes;
-
 /**
- * Para remover atributos ver
- * https://stackoverflow.com/questions/18209233/spring-mvc-how-to-remove-session-attribute
  *
- * @author ftsuda
+ * @author fedts
  */
 @Controller
-@SessionAttributes("itensSelecionados1")
-@RequestMapping("/exemplo-sessao1")
-public class ExemploSessaoController1 {
+@Scope("session")
+@RequestMapping("/exemplo-sessao2")
+public class ExemploSessaoController2 implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
 
     @Autowired
     private ItemService itemService;
 
+    private List<ItemSelecionado> itensSelecionados = new ArrayList<>();
+
     @GetMapping
     public ModelAndView mostrarTela() {
-        return new ModelAndView("exemplo-sessao1").addObject("itens", itemService.findAll());
+        return new ModelAndView("exemplo-sessao2")
+                .addObject("itens", itemService.findAll());
     }
 
     @PostMapping
-    public ModelAndView adicionarItem(
-            @ModelAttribute("itemId") Integer itemId,
-            @ModelAttribute("itensSelecionados1") List<ItemSelecionado> itensSelecionados,
-            RedirectAttributes redirAttr) {
+    public ModelAndView adicionarItem(@ModelAttribute("itemId") Integer itemId, RedirectAttributes redirAttr) {
         Item item = itemService.findById(itemId);
         itensSelecionados.add(new ItemSelecionado(item));
         redirAttr.addFlashAttribute("msg", "Item ID " + item.getId() + " adicionado com sucesso");
-        return new ModelAndView("redirect:/exemplo-sessao1");
+        return new ModelAndView("redirect:/exemplo-sessao2");
     }
 
     @GetMapping("/limpar")
-    public ModelAndView limparSessao(
-            @ModelAttribute("itensSelecionados1") List<ItemSelecionado> itensSelecionados,
-            RedirectAttributes redirAttr) {
+    public ModelAndView limparSessao(RedirectAttributes redirAttr) {
         itensSelecionados.clear();
         redirAttr.addFlashAttribute("msg", "Itens removidos");
-        return new ModelAndView("redirect:/exemplo-sessao1");
+        return new ModelAndView("redirect:/exemplo-sessao2");
     }
 
-    @ModelAttribute("itensSelecionados1")
     public List<ItemSelecionado> getItensSelecionados() {
-        return new ArrayList<>();
+        return itensSelecionados;
+    }
+
+    public void setItensSelecionados(List<ItemSelecionado> itensSelecionados) {
+        this.itensSelecionados = itensSelecionados;
     }
 
     @ModelAttribute("titulo")
     public String getTitulo() {
-        return "Exemplo Sessao 1 - Uso do @SessionAttributes";
+        return "Exemplo Sessao 2 - Uso do @Controller com @Scope(\"session\")";
     }
-
 }

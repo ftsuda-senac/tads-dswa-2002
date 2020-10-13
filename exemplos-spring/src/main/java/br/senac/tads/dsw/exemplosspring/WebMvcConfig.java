840893105339d8215aa512bba4b3782653b72a0d
ftsuda-senac <ftsuda.senac@gmail.com>
2020-10-13 20:46:11 -0300

    Exmeplo i18n básico + validação

---
+++ exemplos-spring/src/main/java/br/senac/tads/dsw/exemplosspring/WebMvcConfig.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.tads.dsw.exemplosspring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ *
+ * @author fedts
+ */
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    // Configuração para usar mensagens i18n junto com Bean Validation
+    @Bean(name = "messageSource")
+    public ReloadableResourceBundleMessageSource messageSource() {
+        ReloadableResourceBundleMessageSource source = new ReloadableResourceBundleMessageSource();
+        source.setBasename("classpath:i18n/mensagens");
+        source.setDefaultEncoding("UTF-8");
+        source.setCacheSeconds(0);
+        return source;
+    }
+
+    @Bean
+    @Override
+    public LocalValidatorFactoryBean getValidator() {
+        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageSource());
+        return bean;
+    }
+
+}

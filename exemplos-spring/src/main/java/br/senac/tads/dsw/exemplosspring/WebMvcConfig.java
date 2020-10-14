9f24ef36332948bdadee8608d15b1c46116f561e
ftsuda-senac <ftsuda.senac@gmail.com>
2020-10-13 22:01:11 -0300

    Exemplo upload

--- exemplos-spring/src/main/java/br/senac/tads/dsw/exemplosspring/WebMvcConfig.java
+++ exemplos-spring/src/main/java/br/senac/tads/dsw/exemplosspring/WebMvcConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
@@ -66,6 +67,12 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/upload-acesso/**")
+                .addResourceLocations("file:///C:/senac/tads/uploads/");
     }
 
 }

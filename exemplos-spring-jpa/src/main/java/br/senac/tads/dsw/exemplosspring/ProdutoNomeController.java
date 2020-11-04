e921fa0ccbaf069e7e0091e39a32198bd34b46e2
ftsuda-senac <ftsuda.senac@gmail.com>
2020-11-03 21:37:52 -0300

    Outros exemplos Queries no Spring Data JPA

---
+++ exemplos-spring-jpa/src/main/java/br/senac/tads/dsw/exemplosspring/ProdutoNomeController.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.tads.dsw.exemplosspring;
+
+import br.senac.tads.dsw.exemplosspring.produto.Produto;
+import br.senac.tads.dsw.exemplosspring.produto.ProdutoRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ *
+ * @author fedts
+ */
+@Controller
+@RequestMapping("/produto/nome")
+public class ProdutoNomeController {
+   
+    @Autowired
+    private ProdutoRepository repository;
+    
+    @GetMapping
+    public ModelAndView buscarPorNome(@RequestParam("nome") String nome) {
+        List<Produto> resultados = repository.buscaPorNomeSql(nome);
+        ModelAndView mv = new ModelAndView("produto/produto-nome");
+        mv.addObject("resultados", resultados);
+        return mv;
+    }
+}
--- exemplos-spring-jpa/src/main/java/br/senac/tads/dsw/exemplosspring/produto/ProdutoRepository.java
+++ exemplos-spring-jpa/src/main/java/br/senac/tads/dsw/exemplosspring/produto/ProdutoRepository.java
@@ -6,16 +6,29 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-    
+
     // Sintaxe para criar nomes dos métodos do Spring Data JPA:
     // https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.query-methods.query-creation
-    
-    Optional<Produto> findByNome(String nome);
-    
+    List<Produto> findByNome(String nome);
+
+    List<Produto> findByNomeIgnoreCase(String nome);
+
+    List<Produto> findByNomeContainingIgnoreCase(String nome);
+
+    // USAR JPQL COM Spring Data JPA
+    @Query("SELECT p FROM Produto p WHERE upper(p.nome) LIKE UPPER('%'||:termoBusca||'%')")
+    List<Produto> buscaPorNomeJpql(@Param("termoBusca") String nome);
+
+    // USAR SQL NATIVO COM Spring Data JPA
+    @Query(value = "SELECT * FROM PRODUTO WHERE upper(nome) LIKE upper('%'||:termoBusca||'%')", nativeQuery = true)
+    List<Produto> buscaPorNomeSql(@Param("termoBusca") String nome);
+
     // https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#repositories.query-methods.query-property-expressions
     Page<Produto> findAllByCategorias_IdIn(List<Integer> ids, Pageable page);
 

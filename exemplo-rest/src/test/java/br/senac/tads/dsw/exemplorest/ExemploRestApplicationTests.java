b28fc6ca5e4dbe38a52676640c2284a1488506d2
ftsuda-senac <ftsuda.senac@gmail.com>
2020-11-23 19:16:42 -0300

    Exemplos Webservices REST

--- exemplo-rest/src/test/java/br/senac/tads/dsw/exemplorest/ExemploRestApplicationTests.java
+++ exemplo-rest/src/test/java/br/senac/tads/dsw/exemplorest/ExemploRestApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ExemploRestApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
